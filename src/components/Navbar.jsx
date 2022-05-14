e8a764d53a4557fe95679ee2b0fa7429594d7002
marik <gaurav.marik@gmail.com>
2022-05-14 00:41:21 -0400

    navbar, tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsPerson } from "react-icons/bs";
 import { BiSearch } from "react-icons/bi";
-// import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import {
 	FaFacebook,
@@ -12,10 +12,18 @@ import {
 } from "react-icons/fa";
 
 const Navbar = () => {
+	const [nav, setNav] = useState(false);
+	const [logo, setLogo] = useState(false);
+	const handleNav = () => {
+		setNav(!nav);
+		setLogo(!logo);
+	};
 	return (
 		<div className="flex justify-between items-center h-20 px-4">
 			<div>
-				<h1>BEACHES.</h1>
+				<h1 onClick={handleNav} className={logo ? "hidden" : "block"}>
+					BEACHES.
+				</h1>
 			</div>
 			<ul className="hidden md:flex">
 				<li>Home</li>
@@ -25,24 +33,35 @@ const Navbar = () => {
 				<li>Book</li>
 			</ul>
 			<div className="hidden md:flex">
-				<BiSearch size={20} />
+				<BiSearch className="mr-2" size={20} />
 				<BsPerson size={20} />
 			</div>
 
 			{/* Hamburger */}
-			<div className="md:hidden">
-				<HiOutlineMenuAlt4 size={20} />
+			<div onClick={handleNav} className="md:hidden z-10">
+				{nav ? (
+					<AiOutlineClose className="text-black" size={20} />
+				) : (
+					<HiOutlineMenuAlt4 size={20} />
+				)}
 			</div>
 
 			{/* Mobile menu dropdown */}
-			<div className="absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex-col">
+			<div
+				onClick={handleNav}
+				className={
+					nav
+						? "absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex-col"
+						: "absolute left-[-100%]"
+				}
+			>
 				<ul>
 					<h1>BEACHES.</h1>
-					<li>Home</li>
-					<li>Destinations</li>
-					<li>Travel</li>
-					<li>View</li>
-					<li>Book</li>
+					<li className="border-b">Home</li>
+					<li className="border-b">Destinations</li>
+					<li className="border-b">Travel</li>
+					<li className="border-b">View</li>
+					<li className="border-b">Book</li>
 					<div className="flex flex-col">
 						<button className="my-6">Search</button>
 						<button>Account</button>
